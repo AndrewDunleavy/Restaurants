c301ef2cb2a9e74afa6df41c59d4775fb7fb5dd6
Andrew Dunleavy <andrew.dunleavy@thebeansgroup.com>
2022-03-17 14:34:47 +0000

    items added

--- items.js
+++ items.js
@@ -7,11 +7,35 @@ class Item {
     static init = function () {
         db.prepare('CREATE TABLE IF NOT EXISTS items (ID  INTEGER PRIMARY KEY, name TEXT,  price TEXT);').run()
     }
-    constructor(name, price, allergens){
+    constructor(menu_id, name, price,){
+        this.menu_id = menu_id
         this.name = name 
         this.price = price 
-        this.allergens = allergens
+        
+        if (id){
+            // SQL -> Javascript
+            this.id = id
+        } else {
+            // Javascript -> SQL
+            const insert = db.prepare('INSERT INTO items (menu_id,name,price) VALUES (?,?,?);').run(this.menu_id,this.name,this.price);
+            this.id = insert.lastInsertRowid
+        }
+
+        Item.all.push(this)
     }
-}
+    updateItem(updates) {
+        this.menu_id =  updates.menu_id || this.menu_id
+        this.name = updates.name || this.name
+        this.price = updates.price || this.price
+        
+        const update = db.prepare('UPDATE items SET menu_id = ?, name = ?, price = ?')
+        update.run(this.menu_id,this.name,this.price)
+    }
+
+    deleteItem() {
+        db.prepare('DELETE FROM items WHERE id=?;').run(this.id)
+    }
+    }
+
 
 module.exports = Item
--- menu.js
+++ menu.js
@@ -1,13 +1,15 @@
 const path = require("path")
-const items = require("./items.js")
+const Item = require('./item')
 const db = require("better-sqlite3")(path.join(__dirname, 'restaurants.sqlite'))
 class Menu {
+    static all = []
     static init = function() {
         db.prepare('CREATE TABLE IF NOT EXISTS menus (id INTEGER PRIMARY KEY, title TEXT);').run() 
     }
     constructor(restaurant_id, title, id){
         this.restaurant_id = restaurant_id
         this.title = title 
+        this.menuItems = []
         if (id) {
             this.id = id
         } else {
@@ -16,11 +18,29 @@ class Menu {
             const info = insert.run(this.restaurant_id, this.title)
             this.id = info.lastInsertRowid          
         }
-    
+        Menu.all.push(this)
     }
  addItem(item){
      return this.items.push(item)
  }
+
+ updateMenu(updates) {
+
+    this.title = updates.title || this.title
+    this.restaurant_id = updates.restaurant_id || this.restaurant_id
+    this.menuItems = updates.menuItems|| this.menuItems
+
+    const update = db.prepare('UPDATE menus SET restaurant_id =?, title =? WHERE id = ?;')
+    update.run(this.restaurant_id,this.title,this.id)
+}
+
+ deleteMenu() {
+    db.prepare('DELETE FROM menus WHERE id=?;').run(this.id)
+    const index = Menu.all.indexOf(this)
+    Menu.all.splice(index, 1)
+}
+
+
 }
 
 module.exports = Menu
