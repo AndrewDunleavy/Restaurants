40b4ac9a6c62bd812d8759ac58980d009c188555
Andrew Dunleavy <andrew.dunleavy@thebeansgroup.com>
2022-03-17 09:26:33 +0000

    update

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const Restaurant = require("./restaurant")
 const Menu = require("./menu")
 const Item = require("./items")
-Restaurant.init()
-Menu.init()
-Item.init()
+
 
 module.exports = {Restaurant, Menu, Item}
